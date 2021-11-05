8270327d58f17cb18fbad241d430514929667e8a
fmaciasruano <fmaciasruano@gmail.com>
2021-11-05 10:21:12 +0100

    Simple Example

---
+++ WPF_Usage/Application.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+
+namespace WPF_Usage
+{
+    public class WPF_App: Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            WPF_App app = new WPF_App();
+            app.Run();
+        }
+    }
+}
